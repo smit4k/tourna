3f521feff383b29594dabe8330c5c7cdb99c6716
smit4k <smit@smit.codes>
2025-10-05 14:21:52 -0400

    Add Ping command to CommandListener.java

--- src/main/java/codes/smit/listeners/CommandListener.java
+++ src/main/java/codes/smit/listeners/CommandListener.java
@@ -1,5 +1,31 @@
 package codes.smit.listeners;
 
-public class CommandListener {
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 
+public class CommandListener extends ListenerAdapter {
+
+    @Override
+    public void onReady(ReadyEvent event) {
+        // Register commands when bot is ready
+        event.getJDA().updateCommands().addCommands(
+                Commands.slash("ping", "Check the bot's response time")
+        ).queue();
+
+        System.out.println("✅ Commands registered successfully!");
+    }
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        if (event.getName().equals("ping")) {
+            handlePing(event);
+        }
+    }
+
+    private void handlePing(SlashCommandInteractionEvent event) {
+        long time = System.currentTimeMillis();
+        event.reply("Pong!").setEphemeral(true).queue();
+    }
 }
